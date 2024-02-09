4e3a44a3673f464c553c1aeaa7330f60f84b7ee3
YArazov <156554298+YArazov@users.noreply.github.com>
2024-02-09 14:51:55 +0800

    Example lesson 3 hw
    --added draw methods to shapes classes, no need to use instanceof
    --simplified the algorithm for detecting the closest object and storing as moved by mouse using functions in main

--- input.js
+++ input.js
@@ -1,38 +1,35 @@
 import {Vec} from './vector.js';
 
 export class Input {
-    constructor(canv, win) {
+    constructor(canv, win, dt) {
         this.canv = canv;
         this.window = win;
-
+        this.dt = dt;
         this.inputs = {
-            mouse: {
-                position: new Vec(0, 0),
-            },
-            lclick: false,
-            rclick: false
+            mouse: {position: new Vec(0, 0), velocity: new Vec(0, 0), movedObject: null}, 
+            lclick: false, rclick: false, space: false, touches: 0
         };
         
-        //function binding: https://www.geeksforgeeks.org/javascript-function-binding/
-        this.mouseDown = this.mouseDown.bind(this); //fix the meaning of the key word "this" so that it always refers to the Inputs
-        this.mouseUp = this.mouseUp.bind(this); //without biding we get error "Undefined"
+        this.mouseDown = this.mouseDown.bind(this);
+        this.mouseUp = this.mouseUp.bind(this);
         this.onContextMenu = this.onContextMenu.bind(this);
         this.mouseMove = this.mouseMove.bind(this);
         this.resizeCanvas = this.resizeCanvas.bind(this);
     }
 
     addListeners() {
-        this.canv.addEventListener("mousedown", this.mouseDown);    //listens for clicking a mouse button
-        this.canv.addEventListener("mouseup", this.mouseUp);    //listens for releasing the mouse button
-        this.canv.addEventListener('contextmenu', this.onContextMenu);  //right click gives us context menu
-        this.canv.addEventListener('mousemove', this.mouseMove);    //listens for movement of the mouse cursor and stores the coordinates
+        this.canv.addEventListener("mousedown", this.mouseDown);
+        this.canv.addEventListener("mouseup", this.mouseUp);
+        this.canv.addEventListener('contextmenu', this.onContextMenu);
+        this.canv.addEventListener('mousemove', this.mouseMove);
         this.window.addEventListener('resize', this.resizeCanvas, false);
     }
 
     mouseDown(e) {
-        if (e.button==0) {  //0 value means the left button was clicked
+        if (e.button==0) {
             this.inputs.lclick = true;
-        } else if (e.button==2)	{   //2 value means right click
+            
+        } else if (e.button==2)	{
             this.inputs.rclick = true;
         }
     }
@@ -44,16 +41,29 @@ export class Input {
             this.inputs.rclick = false;
         }
     }
-
+    
     onContextMenu(e) {
         e.preventDefault();
     }
 
     mouseMove(e) {
+        this.window.clearTimeout(this.inputs.mouseTimer);
+
         const x = e.pageX - this.canv.offsetLeft;
         const y = e.pageY - this.canv.offsetTop;
+
+        const dx = x - this.inputs.mouse.position.x;
+        const dy = y - this.inputs.mouse.position.y;
+        this.inputs.mouse.velocity.x = dx / this.dt;
+        this.inputs.mouse.velocity.y = dy / this.dt;
+
         this.inputs.mouse.position.x = x;
         this.inputs.mouse.position.y = y;
+        
+        this.inputs.mouseTimer = this.window.setTimeout(function () {
+            this.inputs.mouse.velocity.x = 0; 
+            this.inputs.mouse.velocity.y = 0;
+        }.bind(this), 100);
     }
 
     resizeCanvas() {
--- rigidBody.js
+++ rigidBody.js
@@ -1,14 +1,14 @@
 import {Vec} from './vector.js';
 
-export class RigidBody {    //any object with fixed shape and physics
-    constructor(s) {
-        this.shape = s;
-        this.velocity = new Vec(0, 0);
-        this.isMoved = false;
-    }
+export class RigidBody {
+	constructor(shape) {
+		this.shape = shape;   
+		this.velocity = new Vec(0, 0);
+	}	
 
-    updateShape(dt) {
-        const ds = this.velocity.clone().multiply(dt);  //method chaining
-        this.shape.position.add(ds);
-    }
+	updateShape(dt) {
+		const ds = this.velocity.clone().multiply(dt);  //multiply v * dt = giving you displacement per frame
+		this.shape.position.add(ds);
+    } 
+
 }

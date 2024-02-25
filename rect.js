aba58a4b7874755ce1b57a66919ef9e1a80b558b
YArazov <156554298+YArazov@users.noreply.github.com>
2024-02-25 09:22:23 +0800

    Rectangles can turn

--- rect.js
+++ rect.js
@@ -5,11 +5,14 @@ export class Rect {
 		this.position = pos;
 		this.width = w;
 		this.height = h;
+
+        this.orientation = 0;
 	}
 
 	draw(ctx, strokeColor, fillColor) {
         ctx.save();
         ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(this.orientation);
         if (fillColor) {
             ctx.fillStyle = fillColor;
             ctx.fillRect(
--- rigidBody.js
+++ rigidBody.js
@@ -4,11 +4,15 @@ export class RigidBody {
 	constructor(shape) {
 		this.shape = shape;   
 		this.velocity = new Vec(0, 0);
+
+		this.angularVelocity = 0.1;
 	}	
 
 	updateShape(dt) {
 		const ds = this.velocity.clone().multiply(dt);  //multiply v * dt = giving you displacement per frame
 		this.shape.position.add(ds);
+
+		this.shape.orientation += this.angularVelocity * dt;
     } 
 
 }

ee6918f1ae5f00c8a7e17e41e67e600e3bdcf49d
mahmoud-mohamed-08 <doddawy2008@gmail.com>
2024-04-14 02:57:39 +0300

    Fixed main branch

--- renderer.js
+++ renderer.js
@@ -5,12 +5,34 @@ export class Renderer {
     constructor(canv, ctx) {
         this.canvas = canv;
         this.ctx = ctx;
+        this.renderedAlways = [];
+        this.renderedNextFrame = [];
+        this.texts = [];
     }
     
+    drawText(textObject) {
+        this.ctx.fillStyle = textObject.color;
+        this.ctx.font = textObject.font;
+        this.ctx.fillText(textObject.text, textObject.position.x, textObject.position.y);
+    }
+
     drawFrame(objects, fillCol, bordCol) {
         for (let i = 0; i<objects.length; i++) {
-            objects[i].shape.draw(this.ctx, fillCol, bordCol);
-        } 
+            const shape = objects[i].shape;
+            shape.draw(this.ctx, fillCol, bordCol);
+            shape.aabb.draw(this.ctx, "red");
+        }
+        for (let i = 0; i<this.renderedNextFrame.length; i++) {
+            this.renderedNextFrame[i].draw(this.ctx, bordCol);   //draw each item from the list
+        }
+        this.renderedNextFrame = [];    //clear the array, basically means we only draw them once
+
+        for (let i = 0; i<this.renderedAlways.length; i++) {
+            this.renderedAlways[i].draw(this.ctx, bordCol);
+        }
+        this.texts.forEach(text => {
+            this.drawText(text);
+        });
     }
 
     clearFrame() {
--- vector.js
+++ vector.js
@@ -2,6 +2,7 @@ export class Vec {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+		this.renderOrigin;
 	}
     //chainable methods
 	copy (v) {	//copy the xy of another vector into this
@@ -10,15 +11,45 @@ export class Vec {
 		return this;
 	}
 	
+	setX (x) {
+		this.x = x;
+		return this;
+	}
+
+	setY (y) {
+		this.y = y;
+		return this;
+	}
+
 	add (v) {		//add a vector to this
 		this.x += v.x;
 		this.y += v.y;
+		return this;
+	}
+
+	addX (x) {	//scalar addition
+		this.x += x;
+		return this;
+	}
+	
+	addY (y) {	//scalar addition
+		this.y += y;
 		return this;
 	}
 	
 	subtract (v) {
 		this.x -= v.x;
 		this.y -= v.y;
+		return this;
+	}
+
+	subtractX (x) {
+		this.x -= x;
+		return this;
+	}
+
+	subtractY (y) {
+		this.y -= y;
 		return this;
 	}
 	
@@ -48,6 +79,48 @@ export class Vec {
 		}
 		return this;
 	}
+
+	rotate(angle) {	//in formula angle is Theta
+		const x = this.x;	//Ax
+		const y = this.y;
+		this.x = x * Math.cos(angle) - y * Math.sin(angle);	//Bx
+		this.y = x * Math.sin(angle) + y * Math.cos(angle);
+		return this;
+	}
+
+	rotateCW90() {
+		const x = this.x;
+		this.x = -this.y;
+		this.y = x;
+		return this;
+	}	
+
+	rotateCCW90() {
+		const x = this.x;
+		this.x = this.y;
+		this.y = -x;
+		return this;
+	}	
+
+	invert() {
+		this.x *= -1;
+		this.y *= -1;
+		return this;
+	}
+
+	invertX() {
+		this.x *= -1;
+		return this;
+	}
+
+	invertY() {
+		this.y *= -1;
+		return this;
+	}
+
+	moveDistInDir (dist, dir) {	//dir is a unit vector
+		return this.add(dir.clone().multiply(dist));
+	}
 	
 	//non-chainable
 	clone () {	//create a new vector with xy of this
@@ -60,5 +133,41 @@ export class Vec {
 
 	distanceTo (v) {
 		return this.clone().subtract(v).magnitude();
+	}
+
+	dot(v) {
+		return this.x * v.x + this.y * v.y;
+	}
+
+	angle() {
+		return Math.atan2(this.y, this.x) * 180 / Math.PI;
+	}
+
+	draw(ctx, strokeColor) {
+		if (this.color) {
+			strokeColor = this.color;
+		}
+		if(this.renderOrigin) {
+			const renderEnd = this.renderOrigin.clone().add(this);
+			ctx.beginPath();
+			ctx.moveTo(this.renderOrigin.x, this.renderOrigin.y);
+			ctx.lineTo(renderEnd.x, renderEnd.y);
+			ctx.lineWidth = 3;
+			ctx.strokeStyle = strokeColor;
+			ctx.stroke();
+			ctx.beginPath();
+			ctx.arc(renderEnd.x, renderEnd.y, 5, 0, Math.PI*2, true);	//radius 5
+			ctx.closePath();
+			ctx.strokeStyle = strokeColor;
+			ctx.lineWidth = 3;
+			ctx.stroke();
+		} else {
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, 5, 0, Math.PI*2, true);	//radius 5
+			ctx.closePath();
+			ctx.strokeStyle = strokeColor;
+			ctx.lineWidth = 3;
+			ctx.stroke();
+		}
 	}
 }
